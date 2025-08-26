b3f6d28d2b79bb6dcf97710c5a520939826f323e
Guanggeng Yang <yangguanggeng960123@gmail.com>
2025-08-26 14:01:13 -0700

    fix: update deploy script in package.json and remove base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  base: "/preview-genertiveui.github.io/",
   build: {
     outDir: "docs",
     assetsDir: "assets",
